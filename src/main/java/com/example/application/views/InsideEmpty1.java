6497fe2dbb4b59a6ddba63b1e62323b8f35256ac
Iryna Hrytsiuk <hrytsiuk@codevog.com>
2022-02-18 18:01:58 +0200

    Fix.

--- src/main/java/com/example/application/views/InsideEmpty1.java
+++ src/main/java/com/example/application/views/InsideEmpty1.java
@@ -76,6 +76,7 @@ public class InsideEmpty1 extends VerticalLayout {
             InputStream in = new BufferedInputStream(url.openStream());
             StreamResource streamResource = new StreamResource(imageName, () -> in);
             image.setSrc(streamResource);
+            in.close();
         } catch (Exception exception) {
             exception.printStackTrace();
         }
